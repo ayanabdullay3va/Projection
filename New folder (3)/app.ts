9423c35042fac3fcb03b376383986783300330a1
ayanabdullay3va <bd867CKFS@code.edu.az>
2023-12-12 07:56:03 -0800

    asdasd

--- New folder (3)/app.ts
+++ New folder (3)/app.ts
@@ -1,13 +1,16 @@
 // let counter=document.querySelector(".count")!
 // let plus =document.querySelector(".plusbtn")!
 // let minus=document.querySelector(".minusbtn")!
+
 // plus.addEventListener("click",()=>{
 // counter.textContent=String(+counter.textContent+1)
 // console.log(counter.textContent);
+
 // })
 // minus.addEventListener("click",()=>{
 // if (counter.textContent=String(+counter.textContent-1)) {
 //    console.log(counter.textContent);
+   
 // }
 // })
 // let plus =document.createElement("button")
@@ -22,25 +25,34 @@
 // plus.addEventListener("click",()=>{
 // counter.textContent=String(+counter.textContent+1)
 // console.log(counter.textContent);
+
 // })
 // minus.addEventListener("click",()=>{
 //     if (  counter.textContent=="0") {
+        
 //     }
 // else{
 //     counter.textContent=String(+counter.textContent-1)
 //    console.log(counter.textContent);
+   
 // }
 // })
 // function Square() {
 //    kind:enum , sideLenght:Number,
 // }
-var Shapekind;
-(function (Shapekind) {
-    Shapekind[Shapekind["circle"] = 0] = "circle";
-    Shapekind[Shapekind["square"] = 1] = "square";
-})(Shapekind || (Shapekind = {}));
-var circle = /** @class */ (function () {
-    function circle() {
-    }
-    return circle;
-}());
+
+enum Shapekind{
+    circle, 
+square
+}
+interface Shape{
+    kind:Shapekind,
+    radius:number,
+    sideLenght:number,
+    getArea:(a:number)=>number
+}
+class circle{
+    getArea:(a:number)=>number
+
+    
+}
