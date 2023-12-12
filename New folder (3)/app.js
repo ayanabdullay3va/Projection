cae6f0c280ea25431c2351cb2a982ab0155fdeb3
ayanabdullay3va <bd867CKFS@code.edu.az>
2023-12-12 05:16:39 -0800

    fjdfh

---
+++ New folder (3)/app.js
@@ -0,0 +1,85 @@
+// let arr=[{
+//     name: "Ayan",
+//     age:19 
+// },
+// {
+//     name: "Semaye",
+//     age:19 
+// },
+// {
+//     name: "Fatma",
+//     age:19 
+// }]
+
+// let getMaxAgeMinAge=()=>{
+//     let arr=[]
+//     let max=arr[0].age
+//     let min=arr[0].age
+//     let maxage=arr.map(element =>{
+//         if (max<arr.length) {
+//             max=arr.length
+//             console.log(max);
+//         }
+//         if (min>arr.length){
+//             min=arr.length
+//             console.log(min);
+//         }
+//     })
+//     Arr1=[min,max,max-min]
+//   return Arr1
+// }
+// console.log(getMaxAgeMinAge());
+
+// let= prompt("Sozu daxil edin")
+//String.prototype.Myfirst()
+
+//     return this[0];
+
+
+
+
+
+
+// PRAKTIKA
+// let soz="Salam"
+// String.prototype.capitalize=function(){
+//  return this[0].toUpperCase()
+// }
+// console.log(soz.capitalize());
+
+// task 2
+
+// let soz=""
+// String.prototype.isBlank=function(){
+//     if (this=="") {
+//        return false 
+//     }
+// for (let i = 0; i < this.length; i++) {
+// }
+// if (this==" ") {
+//     return true
+// }
+
+// return true
+// }
+// console.log(soz.isBlank());
+ 
+// task3
+// let soz="Salam"
+// let soz2="sagol"
+// String.prototype.concat=function(){
+//     return soz+soz2
+// }
+// console.log(soz.concat(soz2));
+
+
+// task4
+let soz="selem"
+String.prototype.replace=function(elem1,elem2){
+    for (let i = 0; i < thislrkreterrejeoprmfkujei  lfk.length; i++) {
+    if (this.replace("e","a")) {
+        
+    } 
+    }
+}
+console.log(soz.replace());
